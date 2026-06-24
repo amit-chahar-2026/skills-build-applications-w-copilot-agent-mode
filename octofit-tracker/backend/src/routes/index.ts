b4d25ef990133dacbcf436eaea885e925fa332ea
amit-chahar-2026 <amit.chahar@accenture.com>
2026-06-24 22:05:10 +0000

    Fix backend ESM TypeScript routing and seed validation

--- octofit-tracker/backend/src/routes/index.ts
+++ octofit-tracker/backend/src/routes/index.ts
@@ -1,8 +1,8 @@
-import users from './users';
-import teams from './teams';
-import activities from './activities';
-import leaderboard from './leaderboard';
-import workouts from './workouts';
+import users from './users.js';
+import teams from './teams.js';
+import activities from './activities.js';
+import leaderboard from './leaderboard.js';
+import workouts from './workouts.js';
 
 export {
   users,
